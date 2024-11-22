269529ac68ef852300984e89facda9d496e061a0
Victor Sousa <victor.henriqueoff@gmail.com>
2024-11-21 21:37:57 -0300

    feat: adicionando filtros de requisição

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import "dotenv/config";
+import { HttpExceptionFilter } from "./filters/http-exception.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
